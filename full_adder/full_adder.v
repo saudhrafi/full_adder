bdeb1648e15a3498a430313468159149bcbe6aaf
saudhrafi <saudhrafi777@gmail.com>
2025-12-10 23:54:54 +0530

    Success

---
+++ full_adder/full_adder.v
@@ -0,0 +1,8 @@
+module full_adder(a,b,cin,sum,diff,carry,borrow);
+input a,b,cin;
+output sum,diff,carry,borrow;
+assign sum = (a^b^cin);
+assign carry = (a&b)|(a&cin)|(b&cin);
+assign diff = (a^b^cin);
+assign borrow = (~a&cin)|(~a&b)|(b&cin);
+endmodule
